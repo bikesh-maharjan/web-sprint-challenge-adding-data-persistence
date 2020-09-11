fdb7438f05dda72d922ef7d531e821a0e350caf9
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-11 09:38:49 -0700

    added tasks description

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -1,13 +1,23 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("tasks").insert([
+    {
+      desc: "You can Rock and Roll with this awesome guitar",
+      notes: "",
+      completed: true,
+      project_id: 1,
+    },
+    {
+      desc: "You are the creator and designer for the website. Be creative!",
+      notes: "",
+      completed: false,
+      project_id: 2,
+    },
+    {
+      desc: "The canon camera takes an absolutely stunning photos.",
+      notes: "",
+      completed: false,
+      project_id: 3,
+    },
+  ]);
 };
