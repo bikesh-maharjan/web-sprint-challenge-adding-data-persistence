68e6ec8d7613b0bbbaabc160092221f2da606eae
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-11 10:25:48 -0700

    added projects-model

---
+++ projects/projects-model.js
@@ -0,0 +1,39 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+  getProjects,
+  getResources,
+  getTasks,
+  getTaskForProject,
+  addProject,
+  addResource,
+  addTasks,
+};
+
+function getProjects() {
+  return db("projects");
+}
+
+function getResources() {
+  return db("resources");
+}
+
+function getTasks() {
+  return db("tasks");
+}
+
+function getTaskForProject(id) {
+  return db("id").where({ project_id: id });
+}
+
+function addProject(project) {
+  return db("projects").insert(project);
+}
+
+function addResource(resource) {
+  return db("resources").insert(rescource);
+}
+
+function addTasks() {
+  return db("tasks").insert(task);
+}
