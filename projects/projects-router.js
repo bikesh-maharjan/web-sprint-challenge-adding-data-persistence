3b7057f749d00c60f9662bd44879a08b41853a5a
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-11 10:56:45 -0700

    MVP Met and Stretch Goal Completed I think lol

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -16,4 +16,72 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/resources", (req, res) => {
+  Projects.getResources()
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ errorMessage: "oh oh something is wrong with the server" });
+    });
+});
+
+router.get("/tasks", (req, res) => {
+  Projects.getTasks()
+    .then((task) => {
+      res.status(200).json(task);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ errorMessage: "oh oh something is wrong with the server" });
+    });
+});
+
+router.get("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+
+  Projects.getTaskForProject(id)
+    .then((projectId) => {
+      res.status(200).json(projectId);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ errorMessage: "Oh oh something is wrong with the server" });
+    });
+});
+
+router.post("/", (req, res) => {
+  Projects.addProject(req.body)
+    .then((addproject) => {
+      res.status(201).json(addproject);
+    })
+    .catch((error) => {
+      res.status(400).json({ errorMessage: "Couldn't add the item" });
+    });
+});
+
+router.post("/resources", (req, res) => {
+  Projects.addResource(req.body)
+    .then((resource) => {
+      res.status(201).json(resource);
+    })
+    .catch((error) => {
+      res.status(400).json({ errorMessage: "Couldn't add the item" });
+    });
+});
+
+router.post("/tasks", (req, res) => {
+  Projects.addTasks(req.body)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch((error) => {
+      res.status(400).json({ errorMessage: "Couldn't add the item" });
+    });
+});
+
 module.exports = router;
